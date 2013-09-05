be9fb50b017aaf0b56221387c733f21b7bc895dc
dady-djo <developer.sviridov@gmail.com>
2013-09-06 01:10:46 +0400

    ivy+artifactory started

--- CoolStub/src/main/java/ru/sviridov/coolstub/MainActivity.java
+++ CoolStub/src/main/java/ru/sviridov/coolstub/MainActivity.java
@@ -1,10 +1,13 @@
 package ru.sviridov.coolstub;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
+
+import ru.sviridov.yuilibrary.MainLibActivity;
 
 public class MainActivity extends Activity {
 
@@ -19,6 +22,7 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this,R.string.toast_text,Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, MainLibActivity.class));
             }
         });
     }
